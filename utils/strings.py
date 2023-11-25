9c6851c97d3e76888a09339bbd179da70b54c24a
Adam1302 <96663005+Adam1302@users.noreply.github.com>
2023-11-25 16:13:41 -0500

    Local vs. Remote Setup Simplification and App Description Update (#12)
    * Changing email info os.environ location to ease local vs. remote development
    
    * Simplifying local vs. deployed code switching
    
    * Prepare for deployment
    
    * String changes to update app description (#11)

--- utils/strings.py
+++ utils/strings.py
@@ -1,4 +1,4 @@
 
-site_description = "It is neither widely accessible nor impartially reliable to explore and navigate a three thousand year history of great Art, Media, and Literature simply through recommendations. This tool can be used to identify, summarize, and suggest great works."
+site_description = "It is neither widely accessible nor impartially reliable to explore and navigate a three thousand year history of great Art, Media, and Literature simply through recommendations. This tool can be used to identify, summarize, and suggest great works, as well as to provide various philosophical perspectives related to topics and philsophers of interest."
 
 site_technology_description = "AML harnesses the power Large Language Models (LLMs), a type of artificial intelligence that uses large data sets and complex algorithms to hold, analyze, and generate content."
